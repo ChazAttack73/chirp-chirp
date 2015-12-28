2b5c26b0ad02e2bc3c7103627eb1a5dabdb94756
Chaz <cmtlum73@gmail.com>
2015-12-28 10:06:49 -1000

    setup first test and passed

---
+++ chirp-chirp.js
@@ -0,0 +1,5 @@
+module.exports = chirpChirp;
+
+function chirpChirp () {
+
+}
---
+++ test/chirp-chirp.spec.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
+
+var chirpChirp = require( '../chirp-chirp.js' );
+
+describe( 'chirpChirp', function () {
+  it( 'should exist', function () {
+    expect( chirpChirp ).to.exist;
+    expect( chirpChirp ).to.be.a( 'function' );
+  });
+});
